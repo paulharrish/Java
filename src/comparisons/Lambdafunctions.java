c1ef64a033ab5d0069c711a0f06f139f0194684b
paulharrish <prasannaharrish@gmail.com>
2023-11-01 10:49:15 +0530

    Updated Lambda functions using Generics.

--- src/comparisons/Lambdafunctions.java
+++ src/comparisons/Lambdafunctions.java
@@ -1,8 +1,9 @@
 package comparisons;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
-public class Lambdafunctions {
+public class Lambdafunctions<T> {
     public static void main(String[] args) {
 
         ArrayList<Integer> arr = new ArrayList<>();
@@ -10,11 +11,33 @@ public class Lambdafunctions {
             arr.add(i+1);
         }
 
-        arr.forEach((item) -> {System.out.println(item*2);});
         
+        Consumer<Integer> fun = (item) -> System.out.println(item*2);
+        // arr.forEach(fun);
+
+        Operation<Integer> sum = (a, b) -> (a+b); //declaring a method in an interface in an single line using lambda functions.
+        Operation<Integer> prod = (a, b) -> (a*b); //declaring a method in an interface in an single line using lambda functions.
+        Operation<Integer> diff = (a, b) -> (a-b); //declaring a method in an interface in an single line using lambda functions.
+    
+        
+        //we can store the fun in a single variable using consumer type where consumer is an functional interface;
+        
+        Lambdafunctions<Integer> test = new Lambdafunctions<>();
+        System.out.println(test.operate(13, 14, sum));
+        System.out.println( test.operate(13, 14, diff));
+        System.out.println(test.operate(13, 14, prod));
+
     }
 
-    public Integer add(Integer a, Integer b){
-        return a+b;
+   
+
+    private T operate(T a, T b, Operation op){
+        return  (T) op.operation(a, b);
     }
 }
+@FunctionalInterface  //functional interface of generic type.
+interface Operation<T>{
+    T operation(T a,T b);
+}
+
+/* We can implement the abstract methods defined int the interface using lambda functions. */
