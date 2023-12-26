decb53c593e6156e21338e1fefb90013f57e7ac7
paulharrish <prasannaharrish@gmail.com>
2023-12-26 12:04:19 +0530

    updated

--- src/oopconcepts/polymorphism/Main.java
+++ src/oopconcepts/polymorphism/Main.java
@@ -2,29 +2,27 @@ package oopconcepts.polymorphism;
 
 public class Main {
     public static void main(String[] args) {
-        Shapes shape = new Shapes();
-        Circle circle = new Circle();
-        Square square = new Square();
+        Shapes shape = new Circle();
+        shape.area();
+        Shapes shape1 = new Square();
+        shape1.area();
+        // Circle circle = new Circle();
+        // Square square = new Square();
 
         // shape.area();
         // circle.area();
         // square.area();
 
-        // method overriding.
-        // shapes circle2 = new circle();
-        // shapes square1 = new square();
-        // shapes square3 = new square();
-
-        Main obj = new Main();
-        obj.getArea(circle);
-        obj.getArea(square);
-        obj.getArea(shape);
+        // Main obj = new Main();
+        // obj.getArea(circle);
+        // obj.getArea(square);
+        // obj.getArea(shape);
 
     }
 
-    public void getArea(Shapes shape) {
-        shape.area();
+    // public void getArea(Shapes shape) {
+    // shape.area();
 
-    };
+    // };
 
 }
